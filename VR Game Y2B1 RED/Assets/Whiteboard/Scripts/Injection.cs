8e2f3b07dcd5246772deacba35cbf2094ddfc2f8
MatpuX <mato.pullman@gmail.com>
2025-11-03 14:37:31 +0100

    added more assets fixed issues

--- VR Game Y2B1 RED/Assets/Whiteboard/Scripts/Injection.cs
+++ VR Game Y2B1 RED/Assets/Whiteboard/Scripts/Injection.cs
@@ -10,17 +10,12 @@ public class Injection : MonoBehaviour
     public bool dose1 = false;
     public bool dose2 = false;
     public bool dose3 = false;
-    public bool alreadyDrugged1 = false;
-    public bool alreadyDrugged2 = false;
-    public bool alreadyDrugged3 = false;
-    private bool pressed = false;
     public int durability = 0;
-    public InputActionReference customButton;
     public GameObject injection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        customButton.action.started += Drop;
+        
     }
 
     // Update is called once per frame
@@ -28,11 +23,7 @@ public class Injection : MonoBehaviour
     {
         
     }
-    void Drop(InputAction.CallbackContext context)
-    {
-        pressed = !pressed;
-        Debug.Log("boolean is "+ pressed);
-    }
+    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -45,23 +36,23 @@ public class Injection : MonoBehaviour
                 injection.GetComponent<Drugged>().drug1 = true; 
                 injection.GetComponent<Drugged>().amDrugged(); 
                 Debug.Log("drugged");
-                pressed = false;
+                
                 durability--;
                 }
-                else if (dose2 == true && injection.GetComponent<Drugged>().drugged == false&& pressed == true)
+                else if (dose2 == true && injection.GetComponent<Drugged>().drugged == false)
                 { 
                 injection.GetComponent<Drugged>().drug2 = true;
                 injection.GetComponent<Drugged>().amDrugged();
                 Debug.Log("drugged");
-                pressed = false;
+                
                 durability--;
                 }
-                else if (dose3 == true && injection.GetComponent<Drugged>().drugged == false&& pressed == true)
+                else if (dose3 == true && injection.GetComponent<Drugged>().drugged == false)
                 { 
                 injection.GetComponent<Drugged>().drug3 = true;
                 injection.GetComponent<Drugged>().amDrugged();
                 Debug.Log("drugged");
-                pressed = false;
+                
                 durability--;
                 }
                 if(durability == 0) {Destroy(this.gameObject);}
